bdcd04677c9c328e508d098eb3b67a703ff5d0a1
Fazle <cfazle@gmail.com>
2020-06-17 18:36:04 -0400

    Add project files.

---
+++ Extension/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using LoggerService;
+using Contracts;
+
+namespace SchoolAPI.Extension
+{
+    public  static class ServiceExtensions
+    {
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
